94d2a0e162df3e49cef4e266d272d451b60bba4d
Ruslan Izmaylov <ruslan-93@mail.ru>
2018-12-04 00:03:22 +0300

    Day2p2 with streams

--- src/main/scala/org/sandbox/adventofcode2018/Day2p2.scala
+++ src/main/scala/org/sandbox/adventofcode2018/Day2p2.scala
@@ -7,7 +7,7 @@ import scala.io.Source
   */
 object Day2p2 {
 
-  def main(args: Array[String]): Unit = {
+  def solveWithLoops: Option[String] = {
     val lines = Source.fromFile("input/day2.txt").getLines().toList
 
     def check(id1: String, id2: String): Option[String] =
@@ -28,6 +28,30 @@ object Day2p2 {
       case h :: t => go2(h, t).orElse(go1(t))
     }
 
-    println(go1(lines))
+    go1(lines)
+  }
+
+  def solveWithStreams: Option[String] = {
+
+    def check(id1: String, id2: String): Option[String] =
+      if (id1.length != id2.length) None
+      else {
+        val common = id1.zip(id2).filter { case (c1, c2) => c1 == c2 }.unzip._1.mkString("")
+        if (common.length == id1.length - 1) Some(common)
+        else None
+      }
+
+    val stream = Source.fromFile("input/day2.txt").getLines().toStream
+    val tails = Stream.iterate(stream)(_.tail)
+    tails.map {
+      case h #:: tail =>
+        tail.map(check(h, _)).collectFirst { case Some(res) => res }
+      case _ =>
+        None
+    }.collectFirst { case Some(res) => res}
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(solveWithStreams)
   }
 }
